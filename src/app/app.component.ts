ff169742e6760a299a80834f032107680c46ea79
alex <al3xback@gmail.com>
2023-10-29 13:18:53 +0700

    added first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fmentor-nft-angular-data-binding';
+	project = {
+		name: 'NFT',
+		card: {
+			title: 'Equilibrium #3429',
+			description:
+				'Our Equilibrium collection promotes balance and calm.',
+			ethereumAmount: 0.041,
+			remainingTime: 3,
+			author: {
+				name: 'Jules Wyvern',
+				imageSrc: '/assets/images/avatar.png'
+			}
+		},
+		creator: {
+			name: 'Frontend Mentor',
+			link: 'https://www.frontendmentor.io?ref=challenge'
+		},
+		coder: {
+			name: 'al3xback',
+			gitHubLink: 'https://github.com/al3xback'
+		}
+	};
 }
