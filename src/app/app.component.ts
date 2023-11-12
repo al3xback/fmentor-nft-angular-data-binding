6a8c8ecdd0edd7ab7d5f2b6686ce88798407335c
alex <al3xback@gmail.com>
2023-11-12 08:00:21 +0700

    fixed prop name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,12 @@ export class AppComponent {
 			title: 'Equilibrium #3429',
 			description:
 				'Our Equilibrium collection promotes balance and calm.',
-			imageSrc: '/assets/images/equilibrium.jpg',
+			imageUrl: '/assets/images/equilibrium.jpg',
 			ethereumAmount: 0.041,
 			remainingTime: 3,
 			author: {
 				name: 'Jules Wyvern',
-				imageSrc: '/assets/images/avatar.png'
+				imageUrl: '/assets/images/avatar.png'
 			}
 		},
 		creator: {
